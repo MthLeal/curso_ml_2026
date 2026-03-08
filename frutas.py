ea755e141d2f27690038c6516257d4882f1eedbe
Matheus Leal <matheus.eduardo.leal@gmail.com>
2026-03-08 16:19:18 -0300

    Initializing project files

---
+++ frutas.py
@@ -0,0 +1,40 @@
+#%%
+import pandas as pd
+
+df = pd.read_excel("data/dados_frutas.xlsx")
+df
+
+#%%
+from sklearn import tree
+
+arvore = tree.DecisionTreeClassifier(random_state=42)
+
+#%%
+y = df['Fruta']
+caracteristicas = ['Arredondada', 'Suculenta', 'Vermelha', 'Doce']
+x = df[caracteristicas]
+
+#%%
+y
+# %%
+
+arvore.fit(x, y)
+
+#%%
+arvore.predict([[0, 0, 0, 0]])
+#%%
+import matplotlib.pyplot as plt
+
+
+plt.figure(dpi=300)
+tree.plot_tree(arvore, 
+               feature_names=caracteristicas,
+               class_names=arvore.classes_,
+               filled=True)
+
+#%%
+
+proba = arvore.predict_proba([[1,1,1,1]])[0]
+pd.Series(proba, index=arvore.classes_)
+
+#%%
